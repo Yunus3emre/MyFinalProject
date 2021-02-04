b63526eb4f771cd0e3d18920ef661ed3fc3040e3
Yunus Emre Ayan <yunus-emre49@outlook.com>
2021-02-04 17:34:14 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            Console.WriteLine("Foreach Başladı... ");
+
+            foreach (var product in productManager.GetByUnitPrice(40,100))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+
+            Console.WriteLine("Foreach Bitti !");
+        }
+    }
+}
