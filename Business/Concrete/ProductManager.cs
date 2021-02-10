b2b286527ab454fa0abfb8a140f24a82eee5f0cc
Yunus Emre Ayan <yunus-emre49@outlook.com>
2021-02-10 23:35:09 +0300

    10 şubat 2. commit

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Business.Constants;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
@@ -25,21 +26,23 @@ namespace Business.Concrete
             //business codes
             if (product.ProductName.Length<2)
             {
-                return new ErrorResult("Ürün ismi en az 2 karakter olmalıdır");
+                return new ErrorResult(Messages.ProductNameInvalid);
             }
 
             _IProductDal.Add(product);
 
-            return new SuccesResult("Ürün Eklendi");
+            return new SuccesResult(Messages.ProductAdded);
 
         }
 
-        public List<Product> GetAll()
+        public IDataResult<List<Product>> GetAll()
         {
-            //iş kodları
-            //yetkisi var mı?
+            if (DateTime.Now.Hour==22)
+            {
+                return new ErrorResult();
+            }
 
-            return _IProductDal.GetAll();
+            return new SuccessDataResult<List<Product>>(_IProductDal.GetAll(),true,"Ürünler Listelendi");
         }
 
         public List<Product> GetAllByCategoryId(int id)
