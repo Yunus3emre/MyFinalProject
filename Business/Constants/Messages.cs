68268c811719755af5f79ec76b9c93c1a53323ee
Yunus Emre Ayan <67464029+Yunus3emre@users.noreply.github.com>
2021-03-03 22:12:32 +0300

    0303 update

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,6 +1,8 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -11,8 +13,15 @@ namespace Business.Constants
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
         public static string MaintenanceTime="Sistem Bakımda";
         public static string ProductListed="Ürünler Listelendi";
-        public static string ProductCountOfCategoryId = "ürün kategorisinde 10 eleman var";
-        public static string ProductNameAlredyExists = "ürün ismidaha önce kayıt edilmiş";
+        public static string ProductCountOfCategoryId = "Ürün kategorisinde 10 eleman var";
+        public static string ProductNameAlredyExists = "Ürün ismidaha önce kayıt edilmiş";
         public static string CategoryLimitExceded = "Kategori limiti aşıldı";
+        public static string AuthorizationDenied = "Yetkiniz Yok";
+        internal static User UserNotFound;
+        internal static string UserRegistered;
+        internal static User PasswordError;
+        internal static string SuccessfulLogin;
+        internal static string UserAlreadyExists;
+        internal static string AccessTokenCreated;
     }
 }
