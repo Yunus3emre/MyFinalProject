34476b8d132d85a50781978bf6307afbd2fd44cf
Yunus Emre Ayan <67464029+Yunus3emre@users.noreply.github.com>
2021-03-15 16:58:01 +0300

    1503 update

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -42,6 +42,7 @@ namespace WebAPI
             //services.AddSingleton<IProductDal,EfProductDal>();
 
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddCors();
 
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
@@ -70,6 +71,7 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseCors(builder=>builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
